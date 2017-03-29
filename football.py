8d73f3bf3f660caaec84df8132e2c386a7bcdab4
Samar Srivastava <samarsrivastava44@gmail.com>
2017-03-29 21:06:00 +0530

    Update football.py

--- football.py
+++ football.py
@@ -6,19 +6,15 @@ for i in range(T):
 	N = list(input().upper().split())
 	ball_possesed = N[1]
 	MyArray.append(ball_possesed)
-	print(MyArray)
+	
 	for j in range(int(N[0])):
 		process = list(input().upper().split())
 		if process[0] == "P":
 			ball_possesed = process[1]
 			MyArray.append(ball_possesed)
-			print(MyArray)
+			
 		elif process[0] == "B":
 			MyArray.pop()
-			print(MyArray)
+			
 
 	print("Player {}".format(ball_possesed))
-
-
-
-
