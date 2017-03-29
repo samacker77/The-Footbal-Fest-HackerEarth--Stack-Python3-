89421a4e4b06234d0d1f70d8c1f0d8ba491fe748
Samar Srivastava <samarsrivastava44@gmail.com>
2017-03-29 21:27:22 +0530

    Update football.py

--- football.py
+++ football.py
@@ -1,20 +1,11 @@
-#N - Number of passes
-#T - Test Cases
-T = int(input()) #Testcases
-MyArray = []
-for i in range(T):
-	N = list(input().upper().split())
-	ball_possesed = N[1]
-	MyArray.append(ball_possesed)
-	
-	for j in range(int(N[0])):
-		process = list(input().upper().split())
-		if process[0] == "P":
-			ball_possesed = process[1]
-			MyArray.append(ball_possesed)
-			
-		elif process[0] == "B":
-			MyArray.pop()
-			
-
-	print("Player {}".format(ball_possesed))
+for i in range(int(input())):
+    array=[]
+    n,c=map(int,input().split())
+    array.append(c)
+    for j in range(n):
+        s=input().split()
+        if s[0]=='P':
+            array.append(s[1])
+        if s[0]=='B':
+            array[len(array)-1],array[len(array)-2]=array[len(array)-2],array[len(array)-1]
+    print("Player",array[len(array)-1])
